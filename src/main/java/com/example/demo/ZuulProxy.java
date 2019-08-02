24b05a1c5cc186d1e92e7e8a474f8c637026d404
Ganesaprabu <gnavamanirajan@altimetrik.com>
2019-08-02 10:00:48 +0530

    Adding the Gateway

---
+++ src/main/java/com/example/demo/ZuulProxy.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulProxy extends ZuulFilter{
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		logger.info(RequestContext.getCurrentContext().getRequest().getRequestURI().toString());
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
